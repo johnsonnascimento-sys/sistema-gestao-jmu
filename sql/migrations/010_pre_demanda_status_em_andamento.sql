0d3f5d65605ab9f9193e77909d536737f2f313b1
johnsonnascimento-sys <johnson.nascimento@gmail.com>
2026-03-15 16:26:40 -0300

    Fix status migration ordering

--- sql/migrations/010_pre_demanda_status_em_andamento.sql
+++ sql/migrations/010_pre_demanda_status_em_andamento.sql
@@ -1,3 +1,12 @@
+alter table adminlog.pre_demanda
+  drop constraint if exists ck_pre_demanda_status;
+
+alter table adminlog.pre_demanda_status_audit
+  drop constraint if exists ck_pre_demanda_status_audit_anterior;
+
+alter table adminlog.pre_demanda_status_audit
+  drop constraint if exists ck_pre_demanda_status_audit_novo;
+
 update adminlog.pre_demanda
 set status = 'em_andamento'
 where status in ('aberta', 'associada');
@@ -11,9 +20,6 @@ set status_novo = 'em_andamento'
 where status_novo in ('aberta', 'associada');
 
 alter table adminlog.pre_demanda
-  drop constraint if exists ck_pre_demanda_status;
-
-alter table adminlog.pre_demanda
   add constraint ck_pre_demanda_status
   check (status in ('em_andamento', 'aguardando_sei', 'encerrada'));
 
@@ -21,14 +27,8 @@ alter table adminlog.pre_demanda
   alter column status set default 'em_andamento';
 
 alter table adminlog.pre_demanda_status_audit
-  drop constraint if exists ck_pre_demanda_status_audit_anterior;
-
-alter table adminlog.pre_demanda_status_audit
   add constraint ck_pre_demanda_status_audit_anterior
   check (status_anterior in ('em_andamento', 'aguardando_sei', 'encerrada'));
-
-alter table adminlog.pre_demanda_status_audit
-  drop constraint if exists ck_pre_demanda_status_audit_novo;
 
 alter table adminlog.pre_demanda_status_audit
   add constraint ck_pre_demanda_status_audit_novo
